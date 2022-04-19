0c42d0e58a86e68deae201d79d913c8c3e7e2ba2
aichiro01 <aichiro01@outlook.com>
2022-04-18 22:56:03 -0700

    change

--- sketch.js
+++ sketch.js
@@ -18,11 +18,11 @@ let goText;
 
 function setup() {
   createCanvas(500, 500);
-  
+
   frameRate(80);
-  
+
   myButton = new Clickable(); //Create button
-  
+
   myButton.onPress = function () {
     //When myButton is pressed
     this.color = "#00FF00"; //Change button color
@@ -35,7 +35,7 @@ function setup() {
   myButton.color = "#00FF00";       //Background color of the clickable (hex number as a string)
   myButton.textSize = 30;           //Size of the text (integer)
 myButton.textFont = "monospace"; //Font of the text (string)
-  
+
   myButton.onPress = function(){
    gamestate = "lvl1";
 }
@@ -51,16 +51,16 @@ function draw() {
       break;
     case 'gameover':
       gameOver();
-      
+
   }
-    
+
   myButton.draw();
-  
+
    fill(255);
   if (mouseIsPressed === true){
     stroke(5);
-   line(mouseX, mouseY, pmouseX, pmouseY); 
-  } 
+   line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
 
 function titleScreen() {
@@ -76,11 +76,11 @@ function titleScreen() {
   fill(255);
   textSize(50);
   textAlign(CENTER);
-  text('Can You Draw?', width*0.5, height*0.35); 
+  text('Can You Draw?', width*0.5, height*0.35);
 }
 
 function play() {
-  
+
   noStroke();
   fill(245, 245, 220)
   rect(0, 0, 500, 40);
@@ -92,7 +92,7 @@ function play() {
   rect(20, 40, 440, 150);
   rect(460, 40, 20, 420);
   rect(20, 430, 460, 30);
-  
+
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
@@ -104,12 +104,12 @@ function play() {
     background(255, 255, 255);
     drawStart = true;
   }
-  
-  let currentTime = int(millis()/ 1000);   
+
+  let currentTime = int(millis()/ 1000);
   countDown = timeLimit - currentTime;
   textSize(10);
   text("TIMER: " + countDown, 100, 400);
-  
+
   if (countDown < 0){
     countDown = 0;{
       winloss();
@@ -117,10 +117,11 @@ function play() {
     }
   myButton.locate(-190, -350);
 }
-  
+
 function gameOver() {
   background(goR, goG, goB);
   textAlign(CENTER);
+  noStroke();
   fill(245, 245, 220)
   rect(0, 0, 500, 40);
   rect(0, 460, 500, 40);
@@ -131,7 +132,7 @@ function gameOver() {
   textSize(60);
   text('Results!', width*0.5, height*0.23);
   textSize(15);
-  text('How did you do?', width*0.5, height*0.35); 
+  text('How did you do?', width*0.5, height*0.35);
   textSize(100);
   text(goText, width * 0.5, height * 0.65)
 }
