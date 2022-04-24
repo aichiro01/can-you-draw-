17ccd8e1b4d052e6a3d7a1d62bc76b9498bd7816
aichiro01 <aichiro01@outlook.com>
2022-04-23 22:10:07 -0700

    change

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let gamestate = "title";
 
 let drawStart = false;
 
-let timeLimit = 60;
+let timeLimit = 240;
 
 let countDown;
 
@@ -16,8 +16,26 @@ let goFill;
 
 let goText;
 
+let pic0;
+
+let pic1;
+
+let pic2;
+
+let pic3;
+
+
+
+function preload(){
+  pic0 = loadImage('assets/0.png');
+  pic1 = loadImage('assets/1.png');
+  pic2 = loadImage('assets/2.png');
+  pic3 = loadImage('assets/3.png');
+}
+
 function setup() {
   createCanvas(500, 500);
+  background(255);
 
   frameRate(80);
 
@@ -41,6 +59,12 @@ myButton.textFont = "monospace"; //Font of the text (string)
 }
 }
 
+function keyPressed(){
+  if (key === 'c'){
+    background(255);
+  }
+}
+
 function draw() {
   switch (gamestate){
       case"title":
@@ -49,9 +73,20 @@ function draw() {
       case"lvl1":
       play();
       break;
-    case 'gameover':
+      case"lvl2":
+      play1();
+      break;
+      case"lvl3":
+      play2();
+      break;
+      case"lvl4":
+      play3();
+      break;
+      case"lvl5":
+      play4();
+      break;
+      case 'gameover':
       gameOver();
-
   }
 
   myButton.draw();
@@ -62,6 +97,8 @@ function draw() {
    line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
+
+
 
 function titleScreen() {
   noStroke();
@@ -93,12 +130,143 @@ function play() {
   rect(460, 40, 20, 420);
   rect(20, 430, 460, 30);
 
+image(pic0,45, 200);
+
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
   text('Draw what is shown below', width*0.5, height*0.2);
+  textSize(12);
+  text('Clear canvas each drawing by pressing "C"', width*0.5, height*0.35);
   textSize(10);
-  text('You will only have 60 seconds', width*0.5, height*0.89);
+  text('You will only have 60 seconds per drawing', width*0.5, height*0.89);
+
+
+  if (drawStart == false){
+    background(255, 255, 255);
+    drawStart = true;
+  }
+
+  let currentTime = int(millis()/ 1000);
+  countDown = timeLimit - currentTime;
+  textSize(10);
+  text("TIMER: " + countDown, 100, 400);
+
+  if (countDown < 180){
+    countDown = 0;{
+      gamestate = 'lvl2';}
+    }
+  myButton.locate(-190, -350);
+}
+
+function play1() {
+
+  noStroke();
+  fill(245, 245, 220)
+  rect(0, 0, 500, 40);
+  rect(0, 460, 500, 40);
+  rect(0, 0, 20, 500);
+  rect(480, 0, 20, 500);
+  fill(58);
+  rect(20, 40, 150, 420);
+  rect(20, 40, 440, 150);
+  rect(460, 40, 20, 420);
+  rect(20, 430, 460, 30);
+
+image(pic1,45, 200);
+
+  fill(255, 255, 255);
+  textAlign(CENTER);
+  textSize(25);
+  text('Draw what is shown below', width*0.5, height*0.2);
+  textSize(12);
+  text('Clear canvas each drawing by pressing "C"', width*0.5, height*0.35);
+  textSize(10);
+  text('You will only have 60 seconds per drawing', width*0.5, height*0.89);
+
+  if (drawStart == false){
+    background(255, 255, 255);
+    drawStart = true;
+  }
+
+  let currentTime = int(millis()/ 1000);
+  countDown = timeLimit - currentTime;
+  textSize(10);
+  text("TIMER: " + countDown, 100, 400);
+
+  if (countDown < 120){
+    countDown = 0;{
+      gamestate = 'lvl3';}
+    }
+  myButton.locate(-190, -350);
+}
+
+function play2() {
+
+  noStroke();
+  fill(245, 245, 220)
+  rect(0, 0, 500, 40);
+  rect(0, 460, 500, 40);
+  rect(0, 0, 20, 500);
+  rect(480, 0, 20, 500);
+  fill(58);
+  rect(20, 40, 150, 420);
+  rect(20, 40, 440, 150);
+  rect(460, 40, 20, 420);
+  rect(20, 430, 460, 30);
+
+image(pic2,45, 200);
+
+  fill(255, 255, 255);
+  textAlign(CENTER);
+  textSize(25);
+  text('Draw what is shown below', width*0.5, height*0.2);
+  textSize(12);
+  text('Clear canvas each drawing by pressing "C"', width*0.5, height*0.35);
+  textSize(10);
+  text('You will only have 60 seconds per drawing', width*0.5, height*0.89);
+
+  if (drawStart == false){
+    background(255, 255, 255);
+    drawStart = true;
+  }
+
+  let currentTime = int(millis()/ 1000);
+  countDown = timeLimit - currentTime;
+  textSize(10);
+  text("TIMER: " + countDown, 100, 400);
+
+  if (countDown < 60){
+    countDown = 0;{
+      gamestate = 'lvl4';}
+    }
+  myButton.locate(-190, -350);
+}
+
+function play3() {
+
+  noStroke();
+  fill(245, 245, 220)
+  rect(0, 0, 500, 40);
+  rect(0, 460, 500, 40);
+  rect(0, 0, 20, 500);
+  rect(480, 0, 20, 500);
+  fill(58);
+  rect(20, 40, 150, 420);
+  rect(20, 40, 440, 150);
+  rect(460, 40, 20, 420);
+  rect(20, 430, 460, 30);
+
+image(pic3,45, 200);
+
+  fill(255, 255, 255);
+  textAlign(CENTER);
+  textSize(25);
+  text('Draw what is shown below', width*0.5, height*0.2);
+  textSize(12);
+  text('Clear canvas each drawing by pressing "C"', width*0.5, height*0.35);
+  textSize(10);
+  text('You will only have 60 seconds per drawing', width*0.5, height*0.89);
 
   if (drawStart == false){
     background(255, 255, 255);
@@ -117,6 +285,7 @@ function play() {
     }
   myButton.locate(-190, -350);
 }
+
 
 function gameOver() {
   background(goR, goG, goB);
@@ -145,11 +314,11 @@ function winloss() {
     goG = 255;
     goB = 0;
     goFill = 255;
-    goText = 'You Win!'
+    goText = 'You Can!'
   } else if (result >=5 ) {
     goR = 255;
     goB = 0;
     goG = 0;
     goFill = 0;
-    goText = 'You Lose!';  }
+    goText = 'You Cant!';  }
 }
